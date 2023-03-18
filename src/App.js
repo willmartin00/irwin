e321d06af179abb31bcff3200a6555bd67809229
willmartin00 <wmartin@stevens.edu>
2023-03-18 01:57:32 -0400

    added form-builder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import LoginButton from './components/login';
 import LogoutButton from './components/logout';
 import Profile from './components/profile';
+import FormBuilder from './components/FormBuilder';
 
 function App() {
   return (
@@ -15,6 +16,10 @@ function App() {
         <br></br>
         <div>
           <Profile>Profile</Profile>
+        </div>
+        <br></br>
+        <div>
+          <FormBuilder></FormBuilder>
         </div>
       </header>
     </div>
